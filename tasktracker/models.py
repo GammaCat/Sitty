601322accdc734ea48eec68c60e1bcd3a4d5465f
Denis Petrenko <denis.petrenko.dp@gmail.com>
2017-06-08 22:28:09 +0300

    add Task model

--- tasktracker/models.py
+++ tasktracker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+class TASK_STATES:
+    in_progress = "P"
+    ready = "R"
+
 # Create your models here.
+
+
+class Task(models.Model):
+    TASK_STATES_CHOICES = (
+        (TASK_STATES.in_progress, 'in progress'),
+        (TASK_STATES.ready, 'ready'),
+    )
+    title = models.CharField(max_length=200)
+    estimate = models.DateField(auto_now=True)
+    state = models.CharField(max_length=1, choices=TASK_STATES_CHOICES)
+
