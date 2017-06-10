c43c6539ad071b11d3cfd21bc8731550c53095db
Denis Petrenko <denis.petrenko.dp@gmail.com>
2017-06-10 21:36:36 +0300

    add row-level calculated fields: is_failed() and remaining()

--- tasktracker/models.py
+++ tasktracker/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import date as dt
+
 
 class TASK_STATES:
     in_progress = "P"
@@ -14,8 +16,18 @@ class Task(models.Model):
         (TASK_STATES.ready, 'ready'),
     )
     title = models.CharField(max_length=200)
-    estimate = models.DateField(auto_now=True)
+    estimate = models.DateField()
     state = models.CharField(max_length=1, choices=TASK_STATES_CHOICES)
 
     def ready(self):
         self.state = TASK_STATES.ready
+
+    def is_failed(self):
+
+        return True if self.state == TASK_STATES.in_progress and self.estimate < dt.today() else False
+
+    def remaining(self):
+
+        return self.estimate - dt.today() if self.state == TASK_STATES.in_progress else 0
+
+
