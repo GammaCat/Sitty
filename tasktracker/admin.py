b33862ba41bceeaa01bcc028a4944be989020c46
Denis Petrenko <denis.petrenko.dp@gmail.com>
2017-06-11 18:44:03 +0300

    add models to admin interface

--- tasktracker/admin.py
+++ tasktracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Task
+from .models import RoadMap
+
+
+admin.site.register(Task)
+admin.site.register(RoadMap)
